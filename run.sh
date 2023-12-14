6e929709abd5985619ce86dd980f84eb95d97185
Mohammad Efazati <mohammad@efazati.org>
2023-12-14 18:39:06 +0000

    Fix background image and i3rc

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ ln -s $(realpath ./polybar) ~/.config/
 ln -s $(realpath ./rofi) ~/.config/
 ln -s $(realpath ./dunst) ~/.config/
 ln -s $(realpath ./tmux) ~/.tmux
+ln -s $(realpath ./images) ~/images
 
 ln -s $(realpath ./Xdefaults) ~/.Xdefaults
 ln -s $(realpath ./Xresources) ~/.Xresources
@@ -18,3 +19,14 @@ ln -s /home/efazati/.screenlayout/1mon.sh ~/bin/screen/default.sh
 
 sudo apt install -y fonts-font-awesome
 sudo apt install zsh-autosuggestions zsh-syntax-highlighting zsh
+
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+sudo chmod 777 /etc/wireguard 
+
+# INSERT_YOUR_USERNAME ALL = (root) NOPASSWD: /usr/bin/wg-quick
+# /etc/sudoers.d/wiregaurd
