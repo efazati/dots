f000abecc45819a4c2e247a34e6daecbff512ecf
Mohammad Efazati <mohammad@efazati.org>
2025-04-14 21:13:27 +0100

    Add new source for currency

--- polybar/scripts/currency.sh
+++ polybar/scripts/currency.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-URL="https://www.bon-bast.com/" 
-currency_rates=$(curl -s "$URL" | xmllint --html --xpath 'string(//*[@id="usd1"])' - 2>/dev/null)
+# Get USD price from mazaneh.net
+URL="https://mazaneh.net/en/currencyprice/USD"
+HEADERS=(
+    -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0'
+    -H 'Accept: */*'
+    -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8'
+    -H 'X-Requested-With: XMLHttpRequest'
+)
+currency_rates=$(curl -s "$URL" "${HEADERS[@]}" -X POST --data 'ScriptManager1=Listcurrency%7CTimer1&__EVENTTARGET=Timer1' | grep -o '<h3>[0-9]*</h3>' | head -n 1 | sed 's/<h3>\(.*\)<\/h3>/\1/')
 
-# Get Bitcoin price
-bitcoin_price=$(curl -s 'https://api.coindesk.com/v1/bpi/currentprice/USD.json' | jq -r '"BTC:\(.bpi.USD.rate | gsub(",";""))"')
+# Get Bitcoin price from Coingecko API
+bitcoin_price=$(curl -s "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd" | jq -r '"BTC:" + (.bitcoin.usd | tostring)' || echo "BTC:0")
 
 # Print results
 echo "\$$currency_rates | $bitcoin_price"
