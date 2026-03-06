5c4b139452dbc31eb7270bc0efc5af8839bdb250
Mohammad Efazati <mohammad@efazati.org>
2026-03-06 17:12:55 +0400

    Add more tz to polybar

--- polybar/scripts/time.sh
+++ polybar/scripts/time.sh
@@ -1,5 +1,4 @@
-#!/bin/bash 
-
+#!/bin/bash
 
 export TZ=America/Los_Angeles
 pdt=`date +%H:%M`
@@ -10,5 +9,11 @@ est=`date +%H:%M`
 export TZ=Asia/Tehran
 ir=`date +%H:%M`
 
-echo "PDT $pdt  |  EST $est  |  IR $ir"
+export TZ=Asia/Muscat
+om=`date +%H:%M`
+
+export TZ=Europe/Dublin
+ie=`date +%H:%M`
+
+echo "%{F#F0C674}PDT%{F-} $pdt  |  %{F#F0C674}EST%{F-} $est  |  %{F#F0C674}IE%{F-} $ie  |  %{F#F0C674}IR%{F-} $ir  |  %{F#F0C674}OM%{F-} $om"
  
